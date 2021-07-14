1d662ccf447db535b234de06ca8118b13e3c27e3
seregaklim <seregaklim@mail.ru>
2021-07-14 21:06:24 +0300

    first commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,21 +2,16 @@
 fun main () {
     val user = "Crazy Frog"
     var userTime = 3500
-
     val timeHours = userTime / 60 / 60
     val timeMinute = userTime / 60
 
-    val hours = when  {
-        timeHours % 10 == 1 && timeHours % 100 !== 11 -> "$timeHours час"
-        timeHours % 10 in 2..4 && timeHours % 100 !in 11..19 -> "$timeHours часа"
-        else -> "$timeHours часов"
-    }
-    val minute = when  {
-        timeMinute % 10 == 1 && timeMinute % 100 !== 11 -> "$timeMinute минуту"
-        timeMinute % 10 in 2..4 && timeMinute % 100 !in 11..19 -> "$timeMinute минуты"
-        else -> "$timeMinute минут"
-    }
-    val agoToText = when (userTime) {
+    val hours = textHour (timeHours)
+    val minute = minuteText(timeMinute,)
+    val agoToText = tutext(userTime, minute, hours)
+
+    println("$user был $agoToText")
+}
+fun  tutext (userTime: Int, minute: String, hours: String): String = when (userTime){
         in 0..60 -> "только что"
         in 61..3600 -> "$minute назад"
         in 3601..86400 -> "$hours назад"
@@ -25,5 +20,16 @@ fun main () {
         259200 -> "давно"
         else -> "давно"
     }
-    println("$user был $agoToText")
+
+fun minuteText (timeMinute: Int,) : String = when  {
+        timeMinute % 10 == 1 && timeMinute % 100 !== 11 -> "$timeMinute минуту"
+        timeMinute % 10 in 2..4 && timeMinute % 100 !in 11..19 -> "$timeMinute минуты"
+        else -> "$timeMinute минут"
+   }
+
+fun textHour(timeHours: Int,) : String = when  {
+       timeHours % 10 == 1 && timeHours % 100 !== 11 ->"$timeHours час"
+        timeHours % 10 in 2..4 && timeHours % 100 !in 11..19 -> "$timeHours часа"
+        else -> "$timeHours часов"
 }
+
