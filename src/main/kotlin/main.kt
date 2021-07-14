e01277d31179abc412103cd6e6a55fe83c23d0d2
seregaklim <seregaklim@mail.ru>
2021-07-14 14:10:05 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+
+fun main () {
+    val user = "Crazy Frog"
+    var userTime = 3500
+
+    val timeHours = userTime / 60 / 60
+    val timeMinute = userTime / 60
+
+    val hours = when  {
+        timeHours % 10 == 1 && timeHours % 100 !== 11 -> "$timeHours час"
+        timeHours % 10 in 2..4 && timeHours % 100 !in 11..19 -> "$timeHours часа"
+        else -> "$timeHours часов"
+    }
+    val minute = when  {
+        timeMinute % 10 == 1 && timeMinute % 100 !== 11 -> "$timeMinute минуту"
+        timeMinute % 10 in 2..4 && timeMinute % 100 !in 11..19 -> "$timeMinute минуты"
+        else -> "$timeMinute минут"
+    }
+    val agoToText = when (userTime) {
+        in 0..60 -> "только что"
+        in 61..3600 -> "$minute назад"
+        in 3601..86400 -> "$hours назад"
+        in 86401..172800 -> "сегодня"
+        in 172801..259200 -> "вчера"
+        259200 -> "давно"
+        else -> "давно"
+    }
+    println("$user был $agoToText")
+}
